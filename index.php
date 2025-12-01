78588642c166f181e1587b81fbe84dd3b7e4101e
BAYU PRATAMA <144308041+bayuatam@users.noreply.github.com>
2025-12-01 14:48:47 +0700

    Update message in index.php for clarity

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "Halo, ini adalah aplikasi PHP Sederhana dari Pipeline Jenkins! untuk memenugi tugas praktikum\n";
+echo "Halo, ini adalah aplikasi PHP Sederhana dari Pipeline Jenkins! untuk memenugi tugas praktikum matakuliah keamanan sistem informasi";
 echo "Update Otomatis via Webhook Berhasil!!!";
 ?>
