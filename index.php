72f5239f3791d2f2980f9009d983d0fb9db30b78
BAYU PRATAMA <144308041+bayuatam@users.noreply.github.com>
2025-12-01 14:20:48 +0700

    Fix message to indicate successful webhook update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "Halo, ini adalah aplikasi PHP Sederhana dari Pipeline Jenkins! untuk memenugi tugas praktikum\n";
-echo "Update Otomatis via Webhook Berhasil!";
+echo "Update Otomatis via Webhook Berhasil!!!";
 ?>
