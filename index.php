6ea292a7c6d0d05e60baf5bff93d0cbbeda5328c
BAYU PRATAMA <144308041+bayuatam@users.noreply.github.com>
2025-12-08 15:44:26 +0700

    Simplify greeting message in index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "Halo, ini adalah aplikasi PHP Sederhana dari Pipeline Jenkins! untuk memenugi tugas praktikum matakuliah keamanan sistem informasi";
+echo "Halo";
 echo "Update Otomatis via Webhook Berhasil!!!";
 ?>
