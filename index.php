bf318872cadbf26bbb97d263dc1d4432f22dcbfc
bayuatam <bayuptma42@gmail.com>
2025-12-08 22:59:24 +0700

    Uji coba deploy otomatis jenkins ke VPS

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
-<?php
-echo "<h1>Halo, ini update dari kelompok KSI!</h1>";
-echo "<p>Anggota: 4 Orang, …</p>";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tugas KSI Kelompok</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        h1 {
+            color: #27ae60;
+        }
+
+        /* Saya ubah warna jadi HIJAU */
+    </style>
+</head>
+
+<body>
+    <h1>Update Otomatis dari Jenkins Berhasil!</h1>
+    <h3>Anggota Kelompok:</h3>
+    <ul>
+        <li>Bayu Pratama (23753009)</li>
+        <li>Agista</li>
+        <li>Annisa Afrilia</li>
+        <li>Rifa Nabela</li>
+    </ul>
+    <p><i>Terakhir diupdate otomatis oleh Jenkins.</i></p>
+</body>
+
+</html>
